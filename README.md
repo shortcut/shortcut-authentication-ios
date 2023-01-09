a4c9a7ecb3deeeab54d6a4d22785a3826211567c
Sheikh Bayazid <sheikh.bayazid@shortcut.no>
2023-01-09 12:08:54 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ShortcutAuthentication
+
+A description of this package.
---
+++ Sources/ShortcutAuthentication/ShortcutAuthentication.swift
@@ -0,0 +1,6 @@
+public struct ShortcutAuthentication {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
