ca606a0cfcb58def96c5a6c894f547c106c46010
Sheikh Bayazid <sheikh.bayazid@shortcut.no>
2023-01-12 14:30:42 +0100

    Remove unused localizedError

--- AppleIdSignIn/Sources/Entities/AppleIdSignInError.swift
+++ AppleIdSignIn/Sources/Entities/AppleIdSignInError.swift
@@ -21,18 +21,3 @@ public enum AppleIdSignInError: Error {
     /// Other error
     case other(Error)
 }
-
-extension AppleIdSignInError: LocalizedError {
-    public var errorDescription: String? {
-        switch self {
-        case .cancelled:
-            return "Authentication Cancelled"
-        case .failedToRetrieveToken:
-            return "Failed to retrieve token"
-        case .other(let error):
-            return error.localizedDescription
-        case .failedToRetrieveCredentials:
-            return "Failed to retrieve credentials"
-        }
-    }
-}
